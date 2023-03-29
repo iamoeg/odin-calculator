430711111bd1337d71e4a61ca6ea690ba09f0a4c
iamoeg <iamoeg@outlook.com>
2023-03-29 17:58:13 +0000

    Add function to perform operation

--- script.js
+++ script.js
@@ -25,3 +25,23 @@ function divide(num1, num2) {
 let num1, num2;
 let operator;
 
+// PERFORM CALCULATION //
+
+function operate(num1, num2, operator) {
+    switch(operator){
+        case "+":
+            return add(num1, num2);
+            break;
+        case "-":
+            return substract(num1, num2);
+            break;
+        case "*":
+            return multiply(num1, num2);
+            break;
+        case "/":
+            return divide(num1, num2);
+            break;
+        default:
+            return "Error: Missing user input"
+    }
+}
